7302ba06f4bbfc9e3f8e020199a8c5eb6d4f43e6
pure-matcha-latte <85114612+pure-matcha-latte@users.noreply.github.com>
2021-06-03 18:47:37 +0800

    Exercises 2.15-2.16

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,7 +3,7 @@ import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
-import axios from "axios";
+import personService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -12,9 +12,7 @@ const App = () => {
   const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then((response) => setPersons(response.data));
+    personService.getAll().then((initialPersons) => setPersons(initialPersons));
   }, []);
 
   const filteredPersons = persons.filter((person) =>
@@ -31,9 +29,11 @@ const App = () => {
     }
 
     const personObject = { name: newName, number: newNumber };
-    setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNumber("");
+    personService.create(personObject).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
+      setNewName("");
+      setNewNumber("");
+    });
   };
 
   return (
---
+++ phonebook/src/services/persons.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = () => axios.get(baseUrl).then((response) => response.data);
+
+const create = (newObject) =>
+  axios.post(baseUrl, newObject).then((response) => response.data);
+
+const service = { getAll, create };
+export default service;
